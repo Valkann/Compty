705aed9311bca8cc9b0e10d56082b3d8287206fe
Valkann <aymeric.besset@gmail.com>
2016-07-04 14:45:46 +0200

    Changement du user BDD
    pour permettre un accès distance. Sécurisation à faire.

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <?php  
+   
+
     //On inclut et on instancie notre classe    
 	require_once('function.php');
 	
     spl_autoload_register('chargerClasse'); // On enregistre la fonction en autoload pour qu'elle soit appelée dès qu'on instanciera une classe non déclarée.
-
-    $bdd = new PDO('mysql:host=localhost;dbname=compty;charset=utf8', 'root', 'cmc71adm');
     
+    try {
+        $bdd = new PDO('mysql:host=book.agnesb.fr;dbname=compty;charset=utf8', 'remoteuser', '1234');
+        }
+    catch (Exception $e) {
+        echo $e->getMessage(),'<br/><br/>';
+        echo ('Problème de connexion à la base de données');
+}
     $application = new application; 
 
     //On contruit la page   
@@ -13,4 +20,5 @@
 
     //On inclut le template qui appel les différents éléments de la page    
     include($application->template);    
+
 ?>
